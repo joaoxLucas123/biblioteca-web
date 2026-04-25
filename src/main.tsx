691180b2964f03f3188208e1c8c755102b091518
João Lucas Pereira Martins <joaolucaspmartins2004@gmail.com>
2026-04-24 22:27:38 -0300

    fix hotjar

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,13 @@ import './index.css'
 import App from './App.tsx'
 import "./styles.css";
 
+// 👇 COLE AQUI
+const script = document.createElement("script");
+script.src = "https://t.contentsquare.net/uxa/03dd975b90d23.js";
+script.async = true;
+document.head.appendChild(script);
+
+// 👇 NÃO MEXE DAQUI PRA BAIXO
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
